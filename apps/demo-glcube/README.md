24adfedf85f60a461e5eb570bab985df1572bef7
Vladimir Looze <woldemar@mimas.ru>
2022-03-28 02:28:40 +0300

    pack demo-glcube into single file
    for considerably better uglify compression

--- apps/demo-glcube/README.md
+++ apps/demo-glcube/README.md
@@ -1,1 +1,5 @@
-Simple demo showcasing mouse tracking
+Simple demo showcasing rotating cube using WebGL.
+Uses internal 600-character matrix library and no engine.
+Fitting it into QR code was not easy nor pretty, some parts of code are not very readable.
+Building more complex WebGL app would likely require different approach (shaders?)
+or more agressive minifying/uglifying
--- apps/demo-glcube/index.js
+++ apps/demo-glcube/index.js
@@ -1,17 +1,102 @@
-var canvas = document.getElementById('gl');
-CW = canvas.width = window.innerWidth;
-CH = canvas.height = window.innerHeight;
-gl = canvas.getContext('webgl');
+/* Cube geometry */
+/* vertex coordinates */
+var C_V = [
+  -1,-1,-1,   1,-1,-1,   1, 1,-1,   -1, 1,-1,
+  -1,-1, 1,   1,-1, 1,   1, 1, 1,   -1, 1, 1,
+  -1,-1,-1,  -1, 1,-1,  -1, 1, 1,   -1,-1, 1,
+   1,-1,-1,   1, 1,-1,   1, 1, 1,    1,-1, 1,
+  -1,-1,-1,  -1,-1, 1,   1,-1, 1,    1,-1,-1,
+  -1, 1,-1,  -1, 1, 1,   1, 1, 1,    1, 1,-1, 
+];
+/* vertex colors */
+var CUBE_COLOR = [
+  0,0,1, 0,0,1, 0,0,1, 0,0,1,
+  0,1,1, 0,1,1, 0,1,1, 0,1,1,
+  1,0,0, 1,0,0, 1,0,0, 1,0,0,
+  1,0,1, 1,0,1, 1,0,1, 1,0,1,
+  1,1,0, 1,1,0, 1,1,0, 1,1,0,
+  1,1,1, 1,1,1, 1,1,1, 1,1,1 
+];
+/* triangle vertex indices */
+var CUBE_INDEX = [
+  0,1,2, 0,2,3, 4,5,6, 4,6,7,
+  8,9,10, 8,10,11, 12,13,14, 12,14,15,
+  16,17,18, 16,18,19, 20,21,22, 20,22,23 
+];
 
-_new_buf = (t, a) => {
-  b = gl.createBuffer();
-  gl.bindBuffer(t, b);
-  gl.bufferData(t, a, gl.STATIC_DRAW);
+/* shortcuts */
+
+var _cos = (a) => Math.cos(a)
+var _sin = (a) => Math.sin(a)
+
+/* predefined matrices */
+
+var _get_matrix_identity = [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+   ]
+
+var _get_matrix_rotate_x = (a) => [
+   1,     0,      0, 0,
+   0, _cos(a), -_sin(a), 0,
+   0, _sin(a),  _cos(a), 0,
+   0,     0,      0, 1
+];
+
+var _get_matrix_rotate_y = (a) => [
+    _cos(a), 0, _sin(a), 0,
+        0, 1,     0, 0,
+   -_sin(a), 0, _cos(a), 0,
+        0, 0,     0, 1
+];
+ 
+var _get_matrix_rotate_z = (a) => [
+   _Mc(a), -_Ms(a), 0, 0,
+   _Mc(a),  _Ms(a), 0, 0,
+        0,       0, 1, 0,
+        0,       0, 0, 1
+];
+
+ /* matrix operations */
+ 
+/* multiply 4-element matrix by vector */
+var matrix_multiply_MV = (m, v) => [
+    (v[0] * m[ 0]) + (v[1] * m[ 4]) + (v[2] * m[ 8]) + (v[3] * m[12]),
+    (v[0] * m[ 1]) + (v[1] * m[ 5]) + (v[2] * m[ 9]) + (v[3] * m[13]),
+    (v[0] * m[ 2]) + (v[1] * m[ 6]) + (v[2] * m[10]) + (v[3] * m[14]),
+    (v[0] * m[ 3]) + (v[1] * m[ 7]) + (v[2] * m[11]) + (v[3] * m[15])
+]
+
+/* multiply two 4-element matrices */
+var matrix_multiply_MM = (a, b) => Array().concat(
+    matrix_multiply_MV(a, [b[ 0], b[ 1], b[ 2], b[ 3]]),
+    matrix_multiply_MV(a, [b[ 4], b[ 5], b[ 6], b[ 7]]),
+    matrix_multiply_MV(a, [b[ 8], b[ 9], b[10], b[11]]),
+    matrix_multiply_MV(a, [b[12], b[13], b[14], b[15]])
+);
+
+/* rotate matrix m by angle a along X */
+var matrix_rotate_x = (m, a) => matrix_multiply_MM(m, _get_matrix_rotate_x(a));
+
+/* rotate matrix m by angle a along Y */
+var matrix_rotate_y = (m, a) => matrix_multiply_MM(m, _get_matrix_rotate_y(a));
+
+var canvas = document.getElementById('gl');
+var CANVAS_WIDTH = canvas.width = window.innerWidth;
+var CANVAS_HEIGHT = canvas.height = window.innerHeight;
+var GL_CTX = canvas.getContext('webgl');
+
+var _create_buffer = (gl_type, data_array) => {
+  var b = GL_CTX.createBuffer();
+  GL_CTX.bindBuffer(gl_type, b);
+  GL_CTX.bufferData(gl_type, data_array, GL_CTX.STATIC_DRAW);
   return b;
 }
-var BUF_V = _new_buf(gl.ARRAY_BUFFER, new Float32Array(C_V));
-var BUF_C = _new_buf(gl.ARRAY_BUFFER, new Float32Array(C_C));
-var BUF_I = _new_buf(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(C_I));
+var BUF_V = _create_buffer(GL_CTX.ARRAY_BUFFER, new Float32Array(C_V));
+var BUF_C = _create_buffer(GL_CTX.ARRAY_BUFFER, new Float32Array(CUBE_COLOR));
+var BUF_I = _create_buffer(GL_CTX.ELEMENT_ARRAY_BUFFER, new Uint16Array(CUBE_INDEX));
 
 var vertCode = 
    'attribute vec3 p;'+ // position
@@ -28,69 +113,66 @@ var fragCode =
   'varying vec3 C;'+
   'void main(void) { gl_FragColor = vec4(C, 1.0); }';
 
-_sp = gl.createProgram();
-_n_shd = (t, c) => {
-  _shd = gl.createShader(t);
-  gl.shaderSource(_shd, c);
-  gl.compileShader(_shd);
-  gl.attachShader(_sp, _shd);
+var shader_program = GL_CTX.createProgram();
+var _new_shader = (shader_type, shader_code) => {
+  _shd = GL_CTX.createShader(shader_type);
+  GL_CTX.shaderSource(_shd, shader_code);
+  GL_CTX.compileShader(_shd);
+  GL_CTX.attachShader(shader_program, _shd);
   return _shd;
 }
-_vS = _n_shd(gl.VERTEX_SHADER, vertCode);
-_fS = _n_shd(gl.FRAGMENT_SHADER, fragCode);
-gl.linkProgram(_sp);
+_new_shader(GL_CTX.VERTEX_SHADER, vertCode);
+_new_shader(GL_CTX.FRAGMENT_SHADER, fragCode);
+GL_CTX.linkProgram(shader_program);
 
-_mtx = gl.getUniformLocation(_sp, "m");
+var shader_param_matrix = GL_CTX.getUniformLocation(shader_program, "m");
 
-_glbb = (b, s) => {
-  gl.bindBuffer(gl.ARRAY_BUFFER, b);
-  _z = gl.getAttribLocation(_sp, s);
-  gl.vertexAttribPointer(_z, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(_z);
+var _bind_buffer = (buffer, variable_name) => {
+  GL_CTX.bindBuffer(GL_CTX.ARRAY_BUFFER, buffer);
+  _z = GL_CTX.getAttribLocation(shader_program, variable_name);
+  GL_CTX.vertexAttribPointer(_z, 3, GL_CTX.FLOAT, false, 0, 0);
+  GL_CTX.enableVertexAttribArray(_z);
 }
-_glbb(BUF_V, "p");
-_glbb(BUF_C, "c");
+_bind_buffer(BUF_V, "p");
+_bind_buffer(BUF_C, "c");
 
-gl.useProgram(_sp);
+GL_CTX.useProgram(shader_program);
 
-_m_prj = (a, as, zMin, zMax) => {
-  var dz = (zMax-zMin);
-  var ang = Math.tan((a)*3.14/360); // Math.tan((angle*.5)*Math.PI/180);
+var _get_projection_matrix = (view_angle, aspect_ratio, zMin, zMax) => {
+  var ang = Math.tan((view_angle)*3.14/360); // Math.tan((angle*.5)*Math.PI/180);
   return [
-      0.5/ang,         0,                 0,   0,
-            0,  as/ang/2,                 0,   0,
-            0,         0,   -(zMax+zMin)/dz,  -1,
-            0,         0, (-2*zMax*zMin)/dz,   0 
+      0.5/ang,                   0,                          0,   0,
+            0,  aspect_ratio/ang/2,                          0,   0,
+            0,                   0,   -(zMax+zMin)/(zMax-zMin),  -1,
+            0,                   0, (-2*zMax*zMin)/(zMax-zMin),   0 
     ];
 }
+var matrix_projection = _get_projection_matrix(40, CANVAS_WIDTH/CANVAS_HEIGHT, 1, 100);
+var matrix_model = _get_matrix_identity
+var matrix_view = _get_matrix_identity
+matrix_view[14] = -3; /* -5 units back off zero so we can see origin */
 
-var prj_mtx = _m_prj(40, CW/CH, 1, 100);
-var mdl_mtx = _m_i /* model matrix */
-var vw_mtx = _m_i /* view matrix */
-vw_mtx[14] = -3; /* -5 units back off zero so we can see origin */
+var cube_rotation_angle = 0;
 
-var A = 0;
+var animation = () => {
+  matrix_model = _get_matrix_identity
 
-anim = (time) => {
-  mdl_mtx = _m_i /* model matrix */
+  matrix_model = matrix_rotate_y(matrix_model, cube_rotation_angle);
+  matrix_model = matrix_rotate_x(matrix_model, -cube_rotation_angle);
 
-  mdl_mtx = mrY(mdl_mtx, A);
-  mdl_mtx = mrX(mdl_mtx, -A);
+   GL_CTX.enable(GL_CTX.DEPTH_TEST);
+   GL_CTX.clearColor(0, 0, 0, 1);
+   GL_CTX.clearDepth(1.0);
+   GL_CTX.viewport(0.0, 0.0, CANVAS_WIDTH, CANVAS_HEIGHT);
+   GL_CTX.clear(GL_CTX.COLOR_BUFFER_BIT | GL_CTX.DEPTH_BUFFER_BIT);
 
-   time_old = time; 
-   gl.enable(gl.DEPTH_TEST);
-   gl.clearColor(0, 0, 0, 1);
-   gl.clearDepth(1.0);
-   gl.viewport(0.0, 0.0, CW, CH);
-   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+   GL_CTX.uniformMatrix4fv(shader_param_matrix, false, matrix_multiply_MM(matrix_projection,matrix_multiply_MM(matrix_view, matrix_model)));
 
-   gl.uniformMatrix4fv(_mtx, false, m4MM(prj_mtx,m4MM(vw_mtx, mdl_mtx)));
+   GL_CTX.bindBuffer(GL_CTX.ELEMENT_ARRAY_BUFFER, BUF_I);
+   GL_CTX.drawElements(GL_CTX.TRIANGLES, CUBE_INDEX.length, GL_CTX.UNSIGNED_SHORT, 0);
 
-   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, BUF_I);
-   gl.drawElements(gl.TRIANGLES, C_I.length, gl.UNSIGNED_SHORT, 0);
+   cube_rotation_angle += 0.01;
 
-   A += 0.01;
-
-   requestAnimationFrame(anim);
+   requestAnimationFrame(animation);
 }
-anim(0);
+animation();
