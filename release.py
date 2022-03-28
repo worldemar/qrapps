0f16e75bbc8035b66b8baab60e425bd74d5c113b
Vladimir Looze <woldemar@mimas.ru>
2022-03-28 05:54:24 +0300

    Link to index instead of minibundle on pages

--- release.py
+++ release.py
@@ -2,7 +2,7 @@ import os
 
 
 def write_app_info(app_name, readme_file):
-    link = f'### [{app_name}]({app_name}/minibundle.html)\n\n'
+    link = f'### [{app_name}]({app_name}/index.html)\n\n'
     readme_file.write(link.encode('utf-8'))
     app_readme_path = os.path.join('apps', app_name, 'README.md')
     with open(app_readme_path, 'rb') as app_readme:
